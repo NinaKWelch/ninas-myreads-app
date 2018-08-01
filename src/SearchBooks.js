cee4cbf7e59b66cf462c7ae04f15825cb2aa1f1b
ninakwelch <ninakwelch@gmail.com>
2018-08-01 19:39:09 +0100

    feat: add book component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,8 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class SearchBooks extends React.Component {
+	/*state = {
+		books: [],
+		searchTerm: '',
+		searchResults: []
+    }
+
+    handleChange(event) {
+    	this.setState({searchTerm:event.target.value});
+    }
+
+    getResults() {
+        calltodb(searchTerm).then(event => {
+            this.setState({searchResults: event.value})
+        });
+    }*/
 
 	render() {
+		/*const {searchTerm} = this.state*/
 	    return (
 
 		    <div className="search-books">
@@ -11,8 +27,8 @@ class SearchBooks extends React.Component {
 		            <Link className="close-search" to="/">Close</Link>
 		            <div className="search-books-input-wrapper">
 		               { }
-		               <input type="text" placeholder="Search by title or author"/>
-
+		               <input type="text"
+		                   placeholder="Search by title or author" />
 		            </div>
 		        </div>
 		        <div className="search-books-results">
